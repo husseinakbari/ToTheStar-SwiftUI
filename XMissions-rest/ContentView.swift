7c8fea8e13c690c420dc48702cef91a68341d324
Hossein Akbari <mariorover@outlook.com>
2021-06-26 22:22:13 +0430

    Initial Commit

---
+++ XMissions-rest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XMissions-rest
+//
+//  Created by Hossein Akbari on 4/5/1400 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XMissions-rest/XMissions_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMissions_restApp.swift
+//  XMissions-rest
+//
+//  Created by Hossein Akbari on 4/5/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct XMissions_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
